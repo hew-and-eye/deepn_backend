3eb89e48e766da12482e3a45b48135adf44188ad
matthew-bernardo <matthew@e-bot7.com>
2021-03-21 02:16:59 +0100

    Allow passing id to enable put operations

--- functions/create_module.py
+++ functions/create_module.py
@@ -11,23 +11,23 @@ TABLE_NAME = "DEEPN_MODULES"
 def handler(event, context):
     try:
         body = event.get("body")
-        id = str(uuid.uuid4())
+        id = body.get("id", str(uuid.uuid4()))
         dynamodb.put_item(
             TableName=TABLE_NAME,
             Item={
-            "id": _format(id),
-            "name": _format(body['name']),
-            "dependencies": dict_to_item(body['dependencies']),
-            "templates": _format(body['templates']),
-            "owner": _format(body['owner']),
-            "reactions": _format(body['reactions'])
+                "id": _format(id),
+                "name": _format(body["name"]),
+                "dependencies": dict_to_item(body["dependencies"]),
+                "templates": _format(body["templates"]),
+                "owner": _format(body["owner"]),
+                "reactions": _format(body["reactions"]),
             },
         )
         result = {
             "success": True,
             "result": {
                 "id": id,
-                "name": _format(body['name']),
+                "name": _format(body["name"]),
             },
         }
     except Exception as e:
@@ -42,17 +42,11 @@ def _format(value, type="S"):
     elif isinstance(value, str):
         return {type: value}
 
+
 def dict_to_item(raw):
     if isinstance(raw, dict):
-        return {
-            'M': {
-                k: dict_to_item(v)
-                for k, v in raw.items()
-            }
-        }
+        return {"M": {k: dict_to_item(v) for k, v in raw.items()}}
     elif isinstance(raw, list):
-        return {
-            'L': [dict_to_item(v) for v in raw]
-        }
+        return {"L": [dict_to_item(v) for v in raw]}
     elif isinstance(raw, str):
-        return {'S': str(raw)}
+        return {"S": str(raw)}
